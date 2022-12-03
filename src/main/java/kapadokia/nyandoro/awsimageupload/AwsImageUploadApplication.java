e4664c4817fe0a9c87809e6849b16ff3c1e2be17
kapadokia <ktnyandoch@gmail.com>
2022-12-03 07:23:50 +0300

    first commit

---
+++ src/main/java/kapadokia/nyandoro/awsimageupload/AwsImageUploadApplication.java
@@ -0,0 +1,13 @@
+package kapadokia.nyandoro.awsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUploadApplication.class, args);
+	}
+
+}
